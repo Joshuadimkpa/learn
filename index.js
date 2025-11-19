5ad04eefaded52326dc1610738b8b37ee0bc3564
Joshua Dimkpa <joshuadimkpa@gmail.com>
2025-11-19 16:43:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,509 @@
+// console.log("Joshua Dimkpa");
+// numbers
+// Strings
+// Boolean
+
+// 100
+// 10.45
+
+// "My name is Joshua"
+// 'my house number is 44'
+
+// true
+// false
+
+// variables
+
+// var numberStore = 46
+
+// let numberBox = 100
+// const numberRoom = 23
+
+// let number1 = 30;
+// let number2 = 20;
+// let number3 = 10;
+
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+
+// let ageOfStudent = 40;
+// let nameOfStudent = "Johnson Blue";
+// let hasStudentPaid = false;
+// let studentRegNo = "U2012/2592017";
+
+// console.log(ageOfStudent);
+// console.log(nameOfStudent);
+// console.log(hasStudentPaid);
+// console.log(studentRegNo);
+
+// ==== VARIABLE REASSINGMENT =======
+
+// let ourVariable = 100;
+// ourVariable = "Joshua";
+
+// console.log(ourVariable);
+
+// const newNumber = 10;
+// newNumber = 20;
+
+// console.log(newNumber);
+
+//========== ARITHMETIC OPERATIONS ==========
+// OPERATION 1 + 1
+// OPERAND 1
+// OPERATOR +
+
+// additon
+// let result = 1 + 1;
+
+// let oldBalance = 100;
+// let income = 300;
+
+// let newBalance = oldBalance + income;
+
+// console.log(newBalance);
+
+// substraction
+
+// let firstNumber = 300;
+// let secondNumber = 500;
+
+// let result = firstNumber - secondNumber;
+
+// console.log(result);
+
+// Multiplication
+
+// let number1 = 10;
+// let number2 = 10;
+
+// let result = number1 * number2;
+
+// console.log(result);
+
+// DEVITION
+
+// let number1 = 20;
+// let number2 = 30;
+
+// let result = number1 / number2;
+// console.log(result);
+
+// MODULOUS (REMAINDER)
+// let number1 = 7;
+// let number2 = 2;
+
+// let result = number1 % number2;
+// console.log(result);
+
+// EXPONENTIATION (RAISE TO POWER)
+
+// 2 ** 3 2 RAISE TO POWER 3
+
+// let number1 = 2;
+// let number2 = 10;
+
+// let result = number1 ** number2;
+// console.log(result);
+
+// ============ INCREAMENT & DECREMENT ================
+
+// INCREMENTAL OPERATION
+
+// ++ an increament operation
+
+// let number = 10;
+
+// number++;
+
+// console.log(number);
+
+// let newNumber = 20;
+
+// newNumber--;
+
+// console.log(newNumber);
+
+// // ADDITION ASSIGNMENT OPERATION
+// // SUBTRACTION ASSIGNMENT OPERATION
+
+// let mainNumber = 30;
+
+// mainNumber += 10;
+
+// console.log(mainNumber);
+
+// let anotherNumber = 25;
+
+// anotherNumber -= 5;
+
+// console.log(anotherNumber);
+
+// // MULTIPLICATION ASSIGNMENT OPERATION
+// // DIVISION ASSIGNMENT OPERATION
+
+// let gameScore = 200;
+// gameScore *= 10;
+
+// console.log(gameScore);
+
+// let newGameScore = 5000;
+// newGameScore /= 15;
+
+// console.log(newGameScore);
+
+// // COMPARISON OPERATIONS
+
+// let number1 = 10;
+// let number2 = 5;
+
+// less than <
+// let outcome = number1 < number2;
+// greater than >
+// let outcome = number1 > number2;
+
+// let damilolaScore = 400;
+// let tobiScore = 400;
+
+// let outcome = damilolaScore === tobiScore;
+// console.log(outcome);
+
+// let cutOffMark = 200;
+// let joshScore = 250;
+
+// let result = damilolaScore >= cutOffMark;
+// console.log(result);
+
+// let passMark = 50;
+// let studentScore = 30;
+
+// let result = studentScore <= passMark;
+// console.log(result);
+
+// // NOT EQUAL TO OPERATOR
+// let joshScore = 300;
+// let maryScore = 400;
+
+// let outcome = joshScore !== maryScore;
+// console.log(outcome);
+
+// CONDITONAL STATEMENTS
+
+// if STATEMENT, ELSE IF STATEMENT, ELSE STATEMENT
+
+// let studentScore = 450;
+// let cutOffMark = 500;
+
+// if (studentScore >= cutOffMark) {
+//   console.log("Congratulations, You have been admitted");
+// } else {
+//   console.log("Sorry, You have not been admitted");
+// }
+
+// let accountBalance = 20;
+// let cartCost = 500;
+
+// if (cartCost <= accountBalance) {
+//   console.log("purchase successful");
+// // } else {
+// //   console.log("insufficient funds");
+// // }
+
+// // LOGICAL OPERATORS
+
+// // AND OPERATOR (&&)
+// // OR OPERATOR (||)
+// // NOT OPERATOR (!)
+
+// // let cutOffMark = 240;
+// // let age = 18;
+// // let studentScore = 350;
+// // let studentAge = 16;
+
+// // // if (studentScore >= cutOffMark && studentAge >= age) {
+// // //   console.log("Congratulations you have been admitted");
+// // // } else {
+// // //   console.log("Sorry you have not been admitted");
+// // // }
+
+// // if (studentScore >= cutOffMark || studentAge >= age) {
+// //   console.log("Congratulations you have been admitted");
+// // } else {
+// //   console.log("Sorry you have not been admitted");
+// // }
+
+// // ======== STRING OPEARATIONS ==========
+// // lenght operation
+// // let fullName = "Joshua Dimkpa";
+// // let outcome = fullName.length;
+// // console.log(outcome);
+
+// // let passwaord = "llllllll";
+// // let result = passwaord.length;
+// // console.log(result);
+
+// // concatenation operation (Template literals)
+
+// // let studentName = "Prince";
+// // let studentScore = 65;
+
+// // let resultStatement = `Hello ${studentName}, your score is ${studentScore} out of 100.`;
+
+// // console.log(resultStatement);
+
+// // let temperature = 20;
+
+// // let sentense = `The temperature is ${temperature} degree Celsius.`;
+
+// // console.log(sentense);
+
+// let savePassword = "rainfall";
+// let enteredPassword = "rainfall";
+
+// if (enteredPassword === savePassword) {
+//   console.log("Login Successful");
+// } else {
+//   console.log("Login Failed");
+// }
+
+// // OBJECT LITERALS
+
+// /*{
+//     carName : `Honda`,
+//     carColor : `black`,
+//     carmodel : `civic`,
+//     carVin : `HGCRX5678TYU`,
+//     CarYear : 2020,
+// }
+// */
+
+// // how do we storer this data? we use variables to store this data called OBJECT LITERALS.
+
+// /*const carRecord = {
+//   carName: `Honda`,
+//   carColor: `black`,
+//   carmodel: `civic`,
+//   carVin: `HGCRX5678TYU`,
+//   CarYear: 2020,
+// };
+
+// console.log(carRecord);
+// */
+
+// /*const humanRecord = {
+//   humanName: `Linda`,
+//   humanAge: 28,
+//   humanCountry: `Ghana`,
+//   humanHeight: 6.4,
+//   humanGender: `Female`,
+//   humanWeight: `65kg`,
+//   humanOccupation: `Engineer`,
+// };
+
+// console.log(humanRecord);
+// */
+
+// /*const bankCustomer = {
+//   customerName: `Joshua`,
+//   cutomerNationality: `Nigerian`,
+//   customerBVN: 1234567890,
+//   customerPhoneNo: 08034567890,
+//   customerState: `Osun`,
+// };
+
+// console.log(bankCustomer);*/
+
+// /*const studentRecord = {
+//   studentName: `Paul`,
+//   studentDpartment: `Computer Science`,
+//   studentYear: 2023,
+//   studentFaculty: `Science`,
+//   studentRegNo: `U2023/2592017`,
+// };
+
+// console.log(studentRecord);*/
+
+// const carDetials = {
+//   carName: `Toyota`,
+//   carModel: `Corolla`,
+//   carColor: `Black`,
+//   carYear: 2021,
+//   carVin: `TYU5678HGCRX`,
+// };
+
+// // Accessing Object Properties
+// // Dot notation ==== carDetails.carVin
+
+// // let carVinNumber = carDetials.carVin;
+
+// // console.log(carVinNumber);
+
+// // Bracket Notation ==== carDetails['carVin']
+
+// // let carYearMade = carDetials["carYear"];
+
+// // console.log(carYearMade);
+
+// // ADDITION ASSIGNMENT OPERATION/ object property with Dot notation
+
+// // carDetials.millage = 15000;
+
+// // Updating an existing property using Dot notation
+
+// // carDetials.carColor = `White`;
+
+// // Deleting a property from an object literal
+
+// // delete carDetials.carModel;
+
+// // Freezing an Object Literal
+// // Object.freeze(carDetials);
+// // carDetials.carColor = `Red`; // This will not work as the object is frozen
+
+// // sealing an Object Literal
+// Object.seal(carDetials);
+// carDetials.carColor = `Red`; // This will work as the object is sealed
+// carDetials.price = 20000; // This will not work as new properties cannot be added to a sealed object
+
+// console.log(carDetials);
+
+// // SUBTRACTION ASSIGNMENT OPERATION
+
+// const studentRecord = {
+//   fullName: `Joshua`,
+//   age: 14,
+//   studentClass: `JSS2`,
+//   lastSemesterResult: {
+//     mathmathics: `A1`,
+//     english: `B2`,
+//     biology: `A1`,
+//     chemistry: `C4`,
+//     physics: `B3`,
+//   },
+// };
+
+// let outCome = studentRecord.lastSemesterResult.chemistry;
+
+// console.log(outCome);
+
+// const streetRecord = {
+//   streetName: `Park Street`,
+//   streetZone: `Cadastral Zone`,
+//   Council: `municipal`,
+//   houseA: {
+//     houseOwner: `Jacintah`,
+//     houseColor: `Beige`,
+//     houseModel: `Duplex`,
+//     landSize: `450sqm`,
+//   },
+
+//   HouseB: {
+//     houseOwner: `Patricia`,
+//     houseColor: `milk`,
+//     houseModel: `Bungalow`,
+//     landSize: `450sqm`,
+//   },
+
+//   houseC: {
+//     houseOwner: `Paul`,
+//     houseColor: `Skyblue`,
+//     houseModel: `Duplex`,
+//     landSize: `450sqm`,
+//   },
+// };
+
+// // FUNCTIONS DECLARATION
+// /* Function are action blocks */
+
+// let age1 = 32;
+// let age2 = 20;
+// let age3 = 28;
+
+// function averageAge() {
+//   let avarge = (age1 + age2 + age3) / 3; //Bodmas
+//   console.log(avarge);
+// }
+
+// averageAge();
+
+// FUNCTION WITH PARAMETERS
+// ====================================
+
+// function totalPriceOfTwoItems(firstItemPrice, secondItemPrice) {
+//   let totalPrice = firstItemPrice + secondItemPrice;
+//   console.log(totalPrice);
+// }
+
+// Arguments vs Parameters
+// Parameters are the names listed in the function definition.
+// Arguments are the real values passed to the function.
+
+// totalPriceOfTwoItems(200, 100);
+// totalPriceOfTwoItems(45, 90);
+// totalPriceOfTwoItems(320, 98);
+
+// E.G a function that can print a student's mathematics score
+
+// function printScoreReport(studentName, studentScore) {
+//   let scoreReport = `Hello ${studentName}, your mathematics score is ${studentScore}`;
+//   console.log(scoreReport);
+// }
+
+// printScoreReport("Joshua", 85);
+// printScoreReport("Mary", 95);
+// printScoreReport("Paul", 75);
+
+// // Default Parameters
+// function calculateArea(length, width = 10) {
+//   let area = length * width;
+//   console.log(area);
+// }
+
+// calculateArea(5, 20); // width will be 20
+// calculateArea(7);     // width will default to 10
+
+// the return statement
+
+// function printScoreReport(studentName, studentScore) {
+//   let scoreReport = `Hello ${studentName}, your mathematics score is ${studentScore}`;
+
+//   return scoreReport;
+// }
+
+// let outCome = printScoreReport("Joshua", 85);
+
+// console.log(outCome);
+
+// function easy() {
+//   let number1 = 20;
+//   let number2 = 15;
+//   let number3 = 30;
+
+//   let addedValue = number1 + number2 + number3;
+
+//   return addedValue;
+// }
+
+// console.log(easy());
+
+// ===================================
+function averageCalculator(num1, num2, num3, num4) {
+  let average = (num1 + num2 + num3 + num4) / 4;
+
+  return average;
+}
+
+let outcome = averageCalculator(20, 30, 40, 50);
+
+console.log(outcome);
+
+// Block Scope
+// fuction Scope
+// Global Scope
+
+if (10 > 3) {
+  let num1 = 30;
+  let num2 = 10;
+  console.log(num1);
+}
